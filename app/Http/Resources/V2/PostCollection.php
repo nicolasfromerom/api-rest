9a9fc587fbf953dbea914dd80caea365c02c6745
Nicolas Fabricio Romero Morales <romero.nicolasone@gmail.com>
2021-05-05 21:29:48 -0500

    V2 API

---
+++ app/Http/Resources/V2/PostCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\V2;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+
+    public $collects = PostResource::class;
+    
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'meta'=>[
+                'organization'=>'OneKeyCode',
+                'authors'=> [
+                    'Nicolas Romero',
+                    'OneKeyCode'
+                ]
+            ],
+            'type' => 'articles'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::apiResource('v1/post',  App\Http\Controllers\Api\V1\PostController::class)
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+//v1
+Route::apiResource('v1/post',PostV1::class)
     ->only(['index','show','destroy']);
+//v2
+Route::apiResource('v2/post',PostV2::class)
+    ->only(['index','show']);
